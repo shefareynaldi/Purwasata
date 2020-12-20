932a455a892b05fa83f2d7d12075a67f557642d4
shefareynaldi <m.shefareynaldi29@gmail.com>
2020-12-20 23:12:10 +0700

    detail

--- app/src/main/java/com/example/purwasata/ActivityDetail.java
+++ app/src/main/java/com/example/purwasata/ActivityDetail.java
@@ -3,6 +3,10 @@ package com.example.purwasata;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
 
 public class ActivityDetail extends AppCompatActivity {
 
@@ -10,5 +14,12 @@ public class ActivityDetail extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+        TextView nama = findViewById(R.id.nama);
+        TextView kategori = findViewById(R.id.kategori);
+        ImageView gambar = findViewById(R.id.gambar);
+
+        nama.setText(getIntent().getStringExtra("nama"));
+        kategori.setText("Kategori: "+ getIntent().getStringExtra( "kategori"));
+        Glide.with(getApplicationContext()).load(getIntent().getStringExtra("gambar")).into(gambar);
     }
 }
